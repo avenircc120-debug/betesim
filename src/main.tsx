6c5b28f0ba7bd0fd4023b1611a695513fb1dccf2
avenircc120-debug <267755801+avenircc120-debug@users.noreply.github.com>
2026-04-30 10:49:45 +0100

    feat: init Telegram WebApp au démarrage global de l'app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { initSecurityLayer } from "./lib/security";
+import { initTelegramWebApp } from "./lib/telegramWebApp";
 
 initSecurityLayer();
+// Bascule l'app en mode Mini App Telegram (plein écran) si chargée dans Telegram.
+initTelegramWebApp();
 createRoot(document.getElementById("root")!).render(<App />);
